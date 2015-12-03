61644c20a44b0cc767865eb5cd72245a0d467c0f
Terminal-Geek <github@tobias-gehle.de>
2015-12-03 13:02:42 +0100

    Cleaned up first release

---
+++ epg.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import MySQLdb
+from contextlib import closing
+import subprocess
+import urllib
+try:
+    import xml.etree.cElementTree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
+
+import login
+from lamedb import ping
+
+
+TIME = 'e2eventstart'
+DURATION = 'e2eventduration'
+TITLE = 'e2eventtitle'
+TEASER = 'e2eventdescription'
+SUMMARY = 'e2eventdescriptionextended'
+
+
+def check_online(connection, cursor):
+
+    cursor.execute('SELECT host FROM enigma_host WHERE id="1"')
+    host = cursor.fetchone()[0]
+    status = ping(host)
+    if status[1]:
+        exit()
+
+
+def retrieve_epg(connection, cursor, db_result):
+
+    epg = []
+
+    for db_record in db_result:
+
+        alias = db_record[0]
+        url = db_record[1]
+        f = urllib.urlopen(url)
+        epg.append(f.read())
+
+    return epg
+
+
+def parse_epg(epg):
+
+    time = []
+    duration = []
+    title = []
+    teaser = []
+    summary = []
+
+    root = ET.fromstring(epg)
+
+    for element in root.iter(TIME):
+        time.append(element.text)
+    for element in root.iter(DURATION):
+        duration.append(element.text)
+    for element in root.iter(TITLE):
+        title.append(element.text)
+    for element in root.iter(TEASER):
+        teaser.append(element.text)
+    for element in root.iter(SUMMARY):
+        summary.append(element.text)
+
+    return time, duration, title, teaser, summary
+
+
+def add_to_db(connection, cursor,
+              alias, time, duration, title, teaser, summary):
+
+    i = 0
+
+    while i < len(time):
+
+        if not teaser[i]:
+            teaser[i] = ''
+        if not summary[i]:
+            summary[i] = ''
+        summary[i] = summary[i].replace(u'\x8a', u'\n')
+
+        cursor.execute('INSERT INTO epg \
+        (alias, title, teaser, summary, time, duration) \
+        VALUES (%s,%s,%s,%s,%s,%s)',
+                       (alias, title[i], teaser[i],
+                        summary[i], time[i], duration[i]))
+
+        i += 1
+
+    connection.commit()
+
+
+def main():
+
+    with closing(MySQLdb.connect(
+            login.DB_HOST, login.DB_USER,
+            login.DB_PASSWORD, login.DB_DATABASE)) as connection:
+        with closing(connection.cursor()) as cursor:
+
+            check_online(connection, cursor)
+
+            cursor.execute('TRUNCATE TABLE epg')
+            connection.commit()
+            cursor.execute('SELECT alias, url FROM lamedb')
+            db_result = cursor.fetchall()
+
+            epg = retrieve_epg(connection, cursor, db_result)
+
+            i = 0
+
+            for db_record in db_result:
+
+                alias = db_record[0]
+                time, duration, title, teaser, summary = parse_epg(epg[i])
+                add_to_db(connection, cursor,
+                          alias, time, duration, title, teaser, summary)
+
+                i += 1
+
+
+if __name__ == '__main__':
+    main()
---
+++ lamedb.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import MySQLdb
+from contextlib import closing
+import re
+import subprocess
+
+import login
+
+
+LAMEDB_LOCAL = '/home/pi/dreamberry/epg/lamedb'
+RADIO = ':2:0'
+ARD = 'p:ARD'
+ZDF = 'p:ZDFvision'
+
+
+def get_host():
+
+    with closing(MySQLdb.connect(
+            login.DB_HOST, login.DB_USER,
+            login.DB_PASSWORD, login.DB_DATABASE)) as connection:
+        with closing(connection.cursor()) as cursor:
+            cursor.execute('SELECT host FROM enigma_host WHERE id="1"')
+            host = cursor.fetchone()[0]
+
+    return host
+
+
+def ping(host):
+
+    ping = subprocess.Popen(
+        ['ping', '-c 1', host],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    ping.wait()
+    status = ping.communicate()
+
+    return status
+
+
+def fetch_lamedb(lamedb_remote):
+
+    download = subprocess.Popen(['scp', lamedb_remote, LAMEDB_LOCAL])
+    download.wait()
+
+
+def parse_lamedb(lines, host):
+
+    channel = []
+    alias = []
+    url = []
+    i = 0
+    f = 0
+
+    while i < len(lines):
+        if RADIO in lines[i]:
+            if ARD in lines[i+2] or ZDF in lines[i+2]:
+
+                service_id, \
+                    dvb_namespace, \
+                    transport_stream_id, \
+                    original_network_id, \
+                    _, _ = lines[i].split(':', 5)
+
+                url_string = [
+                    'http://',
+                    host,
+                    '/web/epgservice?sRef=1:0:1:',
+                    service_id + ':',
+                    transport_stream_id + ':',
+                    original_network_id + ':',
+                    dvb_namespace,
+                    ':0:0:0:'
+                    ]
+
+                url.append(''.join(url_string))
+                channel.append(
+                    (lines[i+1].decode('utf-8')).strip())
+                alias.append(
+                    re.compile('[^a-zA-Z0-9]').sub('', channel[f].lower()))
+
+                f += 1
+        i += 1
+
+    return channel, alias, url
+
+
+def add_db_entries(channel, alias, url):
+
+    with closing(MySQLdb.connect(
+            login.DB_HOST, login.DB_USER,
+            login.DB_PASSWORD, login.DB_DATABASE)) as connection:
+        with closing(connection.cursor()) as cursor:
+
+            cursor.execute('TRUNCATE TABLE lamedb')
+            connection.commit()
+
+            i = 0
+            while i < len(channel):
+                cursor.execute('INSERT INTO lamedb (channel, alias, url) \
+                VALUES (%s,%s,%s)', ((channel[i]), (alias[i]), (url[i])))
+                i += 1
+            connection.commit()
+
+
+def main():
+
+    host = get_host()
+    lamedb_remote = 'root@' + host + ':/etc/enigma2/lamedb'
+    status = ping(host)
+    if status[1]:
+        exit()
+
+    fetch_lamedb(lamedb_remote)
+
+    with open(LAMEDB_LOCAL, 'r') as f:
+        lines = f.readlines()
+
+    channel, alias, url = parse_lamedb(lines, host)
+
+    add_db_entries(channel, alias, url)
+
+
+if __name__ == '__main__':
+    main()
