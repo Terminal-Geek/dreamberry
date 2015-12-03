052ebdb0ee03b989fe753e74dc0e7e96d105c0f7
Terminal-Geek <github@tobias-gehle.de>
2015-12-03 22:16:38 +0100

    move.py moves files not modified at least since 60 seconds to directory 'finished'

---
+++ move.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import os
+from glob import glob
+import time
+
+DIR_FROM = '/home/pi/dreamberry/recordings/'
+DIR_TO = '/home/pi/dreamberry/recordings/finished/'
+
+
+def mtime(filename):
+    return int(os.path.getmtime(filename))
+
+
+def main():
+
+    now = int(time.time())
+    for f in glob(DIR_FROM+'*.*'):
+        if mtime(f)+60 < now:
+            os.rename(DIR_FROM + os.path.basename(f),
+                      DIR_TO + os.path.basename(f))
+
+
+if __name__ == '__main__':
+    main()
